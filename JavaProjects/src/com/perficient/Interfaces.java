ee0f46559d5b94b9c14bea7276a6bd39f76ade54
prince.muthuselvan <prince.muthuselvan@INDIA-PRINCE.perficient.com>
2019-08-20 18:09:22 +0530

    Sample Program

---
+++ JavaProjects/src/com/perficient/Interfaces.java
@@ -0,0 +1,24 @@
+package com.perficient;
+interface SavingsAccount {
+	  public void savingsaccount(); // interface method
+	}
+
+	interface CurrentAccount {
+	  public void currentaccount(); // interface method
+	}
+
+		class Accounts implements SavingsAccount, CurrentAccount {
+	  public void savingsaccount() {
+	    System.out.println("Savings Account Interest rate is 7%");
+	  }
+	  public void currentaccount() {
+	    System.out.println("Current Account Interest rate is 10%");
+	  }
+	}
+public class Interfaces {
+	public static void main(String[] args) {
+	    Accounts myObj = new Accounts();
+	    myObj.savingsaccount();
+	    myObj.currentaccount();
+}
+}
