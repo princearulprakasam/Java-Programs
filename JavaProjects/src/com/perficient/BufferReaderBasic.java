8cbc2fd1bf2456bb7bba8f23c402c03d24a13c8d
prince.muthuselvan <prince.muthuselvan@INDIA-PRINCE.perficient.com>
2019-08-27 18:12:20 +0530

    Sample Program

--- JavaProjects/src/com/perficient/BufferReaderBasic.java
+++ JavaProjects/src/com/perficient/BufferReaderBasic.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 public class BufferReaderBasic {
 	public static void main(String args[]) throws Exception {
-	FileReader fr=new FileReader("C://Users//prince.muthuselvan//Desktop//Demo.txt");
+	FileReader fr=new FileReader("C://Users//prince.muthuselvan//Desktop//Demo.txt");   //File Location URL
 	BufferedReader br=new BufferedReader(fr);
 	String str=br.readLine();
 	System.out.println(str);
